793b51534d735f21d6059e182ce794210ac676a3
samson472005 <josephsamson064@gmail.com>
2025-02-16 03:13:15 +0530

    renewed version

---
+++ exam.py
@@ -0,0 +1,10 @@
+a = float(input("enter the value ")
+b = float(input("enter the value ")
+          sum = a+b
+          remainder = a-b
+          product = a*b
+          quotent = a/b 
+          print(sum)
+          print(remainder)
+          print(product)
+          print(quotent)
